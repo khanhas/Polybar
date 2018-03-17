e5304ab811f8a712fde08c935fe46ae07ccd15df
Nguyen Xuan Khanh <xuankhanh963@gmail.com>
2018-03-17 20:38:18 +0700

    Added Guild_Metion
    Updated Keybind class

--- Essential Component/DiscordForRainmeter.plugin.js
+++ Essential Component/DiscordForRainmeter.plugin.js
@@ -4,8 +4,8 @@ var DiscordForRainmeter = function () {};
 //You can set this higher if you want
 var MaximumDmUserReturn = 2
 
-unmutedIcon = "background-image: url(\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij4KICA8ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPgogICAgPHJlY3Qgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0Ii8+CiAgICA8cGF0aCBmaWxsPSIjRkZGRkZGIiBkPSJNNyw0LjQ0MDg5MjFlLTE2IEM4LjY1Njg1NDI1LDAgMTAsMS4zNDMxNDU3NSAxMCwzIEwxMCw5IEMxMCwxMC42NTY4NTQyIDguNjU2ODU0MjUsMTIgNywxMiBDNS4zNDMxNDU3NSwxMiA0LDEwLjY1Njg1NDIgNCw5IEw0LDMgQzQsMS4zNDMxNDU3NSA1LjM0MzE0NTc1LDQuNDQwODkyMWUtMTYgNywwIEw3LDQuNDQwODkyMWUtMTYgWiBNMTQsOSBDMTQsMTIuNTMgMTEuMzksMTUuNDQgOCwxNS45MyBMOCwxOSBMNiwxOSBMNiwxNS45MyBDMi42MSwxNS40NCAwLDEyLjUzIDAsOSBMMiw5IEMyLDExLjc2MTQyMzcgNC4yMzg1NzYyNSwxNCA3LDE0IEM5Ljc2MTQyMzc1LDE0IDEyLDExLjc2MTQyMzcgMTIsOSBMMTQsOSBMMTQsOSBaIiB0cmFuc2Zvcm09InRyYW5zbGF0ZSg1IDMpIi8+CiAgPC9nPgo8L3N2Zz4K\");"
-undeafedIcon = "background-image: url(\"data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij4KICA8ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPgogICAgPHJlY3Qgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0Ii8+CiAgICA8cGF0aCBmaWxsPSIjRkZGRkZGIiBkPSJNMywxMSBDMyw2IDcsMiAxMiwyIEwxMiw0IEM4LjEzNDAwNjc1LDQgNSw3LjEzNDAwNjc1IDUsMTEgTDUsMTMgTDksMTMgTDksMjEgTDYsMjEgQzQuMzQzMTQ1NzUsMjEgMywxOS42NTY4NTQyIDMsMTggTDMsMTEgWiBNMjEsMTEgTDIxLDE4IEMyMSwxOS42NTY4NTQyIDE5LjY1Njg1NDIsMjEgMTgsMjEgTDE1LDIxIEwxNSwxMyBMMTksMTMgTDE5LDExIEMxOSw3LjEzNDAwNjc1IDE1Ljg2NTk5MzIsNCAxMiw0IEwxMiwyIEMxNywyIDIxLDYgMjEsMTEgWiIvPgogIDwvZz4KPC9zdmc+Cg==\");"
+unmutedIcon = "background-image: url(\"/assets/4bc527c257233fc69b94342d77bcb9ee.svg\");"
+undeafedIcon = "background-image: url(\"/assets/c7c47afdf35d5a3e06233319d3aa7674.svg\");"
 
 DiscordForRainmeter.prototype.start = function () {
 	updater = setInterval(this.countAgain, 200);
@@ -18,28 +18,35 @@ DiscordForRainmeter.prototype.countAgain = function() {
 
 	//Get user name by finding user id in available channel member list or private chat list
 	function getUserNameByID(id){
-		var users = document.getElementsByClassName("channel-members")[0]
-		if (users){
-			users = users.getElementsByClassName("member")
-			for (var i = 0; i < users.length; i++) {
-				var user = $(users[i].getElementsByClassName("avatar-small"))
+		var userList = document.getElementsByClassName("channel-members")
+		if (userList.length > 0) {
+			userList = userList[0].getElementsByClassName("member")
+			for (var i = 0; i < userList.length; i++) {
+				var user = $(userList[i].getElementsByClassName("avatar-small"))
 				var url = user.css("background-image")
 
 				if (id[0] == url.match(/\d+/)[0])
-					return users[i].getElementsByClassName("member-username-inner")[0].innerText
+					return userList[i].getElementsByClassName("member-username-inner")[0].innerText
 			}
 		}
-		else{
-			users = document.getElementsByClassName("private-channels")[0].getElementsByClassName("channel private")
-			console.log(users)
-			for (var i = 0; i < users.length; i++) {
-				var user = $(users[i].getElementsByClassName("avatar-small"))
-				var url = user.css("background-image")
-				if (id == url.match(/\d+/))
-					return users[i].getElementsByClassName("channel-name")[0].innerText.replace(users[i].getElementsByClassName("channel-activity")[0].innerText,"")	
+		else {
+			userList = document.getElementsByClassName("private-channels")[0].getElementsByClassName("channel private")
+			for (var i = 0; i < userList.length; i++) {
+				var user = userList[i].getElementsByClassName("avatar-small")
+				var url = user[0].style['backgroundImage']
+				if (id[0] == url.match(/\d+/)[0]) {
+					var nameFull = userList[i].getElementsByClassName("channel-name")[0].innerText
+					var namePlaying = userList[i].getElementsByClassName("channel-activity")[0]
+					if (namePlaying) {
+						return nameFull.replace(namePlaying.innerText,"")
+					}
+					else {
+						return nameFull
+					}
+				}
 			}
 		}
-			
+
 		return ""
 	}
 
@@ -52,7 +59,7 @@ DiscordForRainmeter.prototype.countAgain = function() {
 				dmCount = parseInt(badge.innerHTML)
 			else
 				dmCount = 0
-	
+
 			avaClass = $(user.getElementsByClassName("avatar-small"))
 
 			avaLink = avaClass.css("background-image").match(/\"(.*)\"/)[1]
@@ -78,8 +85,29 @@ DiscordForRainmeter.prototype.countAgain = function() {
 
 //UNREAD GUILDS
 	count = document.getElementsByClassName("guild unread").length
-	saveValue("Total_GuildUnread", count)
+	saveValue("Guild_Unread", count)
+//MENTION COUNT
+	//Current channel
+	mention = 0
+	curr_channel = document.getElementsByClassName('iconSpacing-5GIHkT')
+	if (curr_channel.length > 0) {
+		for (i = 0; i < curr_channel.length; i++) {
+			if (curr_channel[i].innerText != "") {
+				mention = mention + parseInt(curr_channel[i].innerText)
+			}
+		}
+	}
 
+	guild = document.getElementsByClassName('guild')
+	for (i = 0; i < guild.length; i++) {
+		if (guild[i].parentElement.className != 'dms') {
+			badge = guild[i].getElementsByClassName('badge')
+			if (badge.length > 0) {
+				mention = mention + parseInt(badge[0].innerText)
+			}
+		}
+	}
+	saveValue("Guild_Mention", mention)
 //DIREC MESSAGES COUNT, AVATAR AND NAME
 	dms = document.getElementsByClassName("dms")[0].getElementsByClassName("guild")
 
@@ -87,7 +115,7 @@ DiscordForRainmeter.prototype.countAgain = function() {
 	for (i = 0; i < MaximumDmUserReturn; i++){
 		TotalDM = TotalDM + getUserDmAva(i)
 	}
-	saveValue("Total_DMCount", TotalDM)
+	saveValue("Direct_Message", TotalDM)
 
 //FRIENDS ONLINE
 	friendOnl = parseInt(document.getElementsByClassName("friends-online")[0].innerHTML.replace("Online",""))
@@ -130,18 +158,37 @@ DiscordForRainmeter.prototype.countAgain = function() {
 		headphoneButton = "Deafed"
 	saveValue("Headphone", headphoneButton)
 
-//KEYBIND COMBINATION 
+//SPEAKING STATUS
+	userAvaLink = document.getElementsByClassName("container-iksrDt")[0].firstChild.getAttribute("style")
+	talkingList = document.getElementsByClassName("draggable-3SphXU")
+	for (i = 0; i < talkingList.length; i++)
+	{
+		checkingAva = talkingList[i].firstChild.firstChild.firstChild
+		if (checkingAva.getAttribute("style") == userAvaLink)
+		{
+			if (checkingAva.className == "avatarSpeaking-3teDH6")
+				saveValue("Speaking", 1)
+			else
+				saveValue("Speaking", 0)
+
+			break
+		}
+	}
+
+//KEYBIND COMBINATION
 //(Only return current combinations when user open "Keybind" in Setting menu. If Keybind setting is not visible, it will just skip but old values are still saved)
-	keybindList = document.getElementsByClassName("keybind-group")
-	if (keybindList) {
+	keybindList = document.getElementsByClassName("keybindGroup-37VaRm")
+	if (keybindList.length > 0) {
 		for (i = 0; i < keybindList.length; i++){
 			selectedValue = keybindList[i].getElementsByClassName("Select-value-label")[0].innerHTML
+
 			if (selectedValue == "Toggle Mute") {
-				hotkey = keybindList[i].getElementsByClassName("input")[0].getAttribute("value")
+
+				hotkey = keybindList[i].getElementsByClassName("input-2ZbpUR")[0].getAttribute("value")
 				saveValue("Toggle_Mute_Keybind", hotkey)
 			}
 			else if (selectedValue == "Toggle Deafen") {
-				hotkey = keybindList[i].getElementsByClassName("input")[0].getAttribute("value")
+				hotkey = keybindList[i].getElementsByClassName("input-2ZbpUR")[0].getAttribute("value")
 				saveValue("Toggle_Deaf_Keybind", hotkey)
 			}
 		}
@@ -175,7 +222,7 @@ DiscordForRainmeter.prototype.getDescription = function () {
 };
 
 DiscordForRainmeter.prototype.getVersion = function () {
-	return "1.0";
+	return "1.1";
 };
 
 DiscordForRainmeter.prototype.getAuthor = function () {
